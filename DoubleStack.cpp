19062fdf9b4d6aca83d1654d670e44c7057f754a
Ark <1150519649@qq.com>
2018-10-23 23:20:44 +0800

    更新 DoubleStack.cpp

--- DoubleStack.cpp
+++ DoubleStack.cpp
@@ -1,3 +1,70 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ERROR 1e8
+typedef int ElementType;
+typedef enum
+{
+	push,
+	pop,
+	end
+} Operation;
+typedef int Position;
+struct SNode
+{
+	ElementType *Data;
+	Position Top1, Top2;
+	int MaxSize;
+};
+typedef struct SNode *Stack;
+
+Stack CreateStack(int MaxSize);
+bool Push(Stack S, ElementType X, int Tag);
+ElementType Pop(Stack S, int Tag);
+
+int main()
+{
+
+	Stack S;
+
+	S = CreateStack(2);
+	printf("%s", "------------\n");
+	Push(S, 4, 1);
+	Push(S, 5, 2);
+	Push(S, 4, 1);
+	Push(S, 5, 2);
+	printf("%d ", Pop(S, 1));
+	printf("%d ", Pop(S, 2));
+	printf("%d ", Pop(S, 1));
+	printf("%d ", Pop(S, 2));
+	printf("%d ", Pop(S, 3));
+	printf("%d ", Pop(S, 4));
+	printf("%s", "------------\n");
+	Push(S, 4, 1);
+	Push(S, 5, 2);
+	printf("%d ", Pop(S, 1));
+	printf("%d ", Pop(S, 2));
+	printf("%d ", Pop(S, 1));
+	printf("%d ", Pop(S, 2));
+	printf("%d ", Pop(S, 3));
+	printf("%d ", Pop(S, 4));
+	printf("%s", "------------\n");
+	Push(S, 4, 1);
+	Push(S, 5, 1);
+	Push(S, 6, 1);
+	printf("%d ", Pop(S, 1));
+	printf("%d ", Pop(S, 2));
+	printf("%d ", Pop(S, 1));
+	printf("%d ", Pop(S, 2));
+	printf("%d ", Pop(S, 3));
+	printf("%d ", Pop(S, 4));
+
+
+	return 0;
+}
+
+/* 你的代码将被嵌在这里 */
 Stack CreateStack(int MaxSize)
 {
 	Stack s = (Stack)malloc(sizeof(struct SNode));
