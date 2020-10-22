f29d3b608b1102be5b5b9fb8e31c0d518f9b8866
XuChuanpeng <xcp@Admin03-Mac.lan>
2020-10-22 11:58:52 +0800

    add mindeepth

--- minDepth.cpp
+++ minDepth.cpp
@@ -1,3 +1,4 @@
+#include <queue>
 struct TreeNode
 {
     int val;
@@ -13,6 +14,70 @@ class Solution
 public:
     int minDepth(TreeNode *root)
     {
-        
+        if (root == nullptr)
+        {
+            return 0;
+        }
+
+        std::queue<TreeNode *> q;
+        q.push(root);
+        TreeNode *b = root;
+        TreeNode *p = root;
+        int n = 1;
+        while (!q.empty())
+        {
+            p = q.front();
+            q.pop();
+            if (p->left == nullptr && p->right == nullptr)
+            {
+                break;
+            }
+            if (p->left != nullptr)
+            {
+                q.push(p->left);
+            }
+            if (p->right != nullptr)
+            {
+                q.push(p->right);
+            }
+
+            if (p == b)
+            {
+                n += 1;
+                b = q.back();
+            }
+        }
+        return n;
+    }
+
+    int getDepth(TreeNode *root, int n)
+    {
+
+        if (root->left == nullptr && root->right == nullptr)
+        {
+            return n;
+        }
+        int a = 100000, b = 100000;
+        if (root->left != nullptr)
+        {
+            a = getDepth(root->left, n + 1);
+        }
+
+        if (root->right != nullptr)
+        {
+            b = getDepth(root->right, n + 1);
+        }
+
+        return std::min(a, b);
+    }
+
+    int minDepth2(TreeNode *root)
+    {
+        if (root == nullptr)
+        {
+            return 0;
+        }
+
+        return getDepth(root, 1);
     }
 };
