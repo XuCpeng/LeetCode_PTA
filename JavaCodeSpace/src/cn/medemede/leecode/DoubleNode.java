cde913383d18bb6e15fae8e631ecdb6320fbb143
Stardust <saber3555@outlook.com>
2021-04-14 08:48:39 +0800

    addd LRUCache
    Manual implementation LRUCache

---
+++ JavaCodeSpace/src/cn/medemede/leecode/DoubleNode.java
@@ -0,0 +1,13 @@
+package cn.medemede.leecode;
+
+class DoubleNode {
+    int key;
+    int val;
+    DoubleNode next;
+    DoubleNode prev;
+
+    public DoubleNode(int key, int val) {
+        this.key = key;
+        this.val = val;
+    }
+}
---
+++ JavaCodeSpace/src/cn/medemede/leecode/LRUCache.java
@@ -0,0 +1,54 @@
+package cn.medemede.leecode;
+
+import java.util.HashMap;
+
+public class LRUCache {
+    private HashMap<Integer, DoubleNode> map;
+    private DoubleList cache;
+    private int cap;
+
+    public LRUCache(int cap) {
+        map = new HashMap<>();
+        cache = new DoubleList();
+        this.cap = cap;
+    }
+
+    private void addRecently(int key, int val) {
+        DoubleNode tmp = new DoubleNode(key, val);
+        cache.addLast(tmp);
+        map.put(key, tmp);
+    }
+
+    private void deleteByKey(int key) {
+        cache.remove(map.get(key));
+        map.remove(key);
+    }
+
+    private void removeLastRecently() {
+        DoubleNode lastRecentlyNode = cache.removeFirst();
+        map.remove(lastRecentlyNode.key);
+    }
+
+    private void makeAsRecently(int key) {
+        DoubleNode tmp = map.get(key);
+        cache.remove(tmp);
+        cache.addLast(tmp);
+    }
+
+    public Integer get(int key) {
+        if (map.containsKey(key)) {
+            makeAsRecently(key);
+            return map.get(key).val;
+        }
+        return null;
+    }
+
+    public void put(int key, int val) {
+        if (map.containsKey(key)) {
+            deleteByKey(key);
+        } else if (cache.getSize() >= cap) {
+            removeLastRecently();
+        }
+        addRecently(key, val);
+    }
+}
