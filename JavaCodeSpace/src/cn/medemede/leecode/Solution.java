e3d5c679a99de55fd99b142c6fbef01df28164a9
Stardust <saber3555@outlook.com>
2021-04-27 22:23:27 +0800

    俄罗斯套娃

--- JavaCodeSpace/src/cn/medemede/leecode/Solution.java
+++ JavaCodeSpace/src/cn/medemede/leecode/Solution.java
@@ -1412,6 +1412,104 @@ public class Solution {
         return dp[t1.length][t2.length];
     }
 
+    /**
+     * 最长递增子序列
+     * <p>动态规划</p>
+     *
+     * @param nums
+     * @return
+     */
+    public int lengthOfLIS(int[] nums) {
+        int max = 0;
+        int[] dp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j]);
+                }
+            }
+            dp[i]++;
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+
+    /**
+     * 最长递增子序列
+     * <p>二分查找，耐心排序，扑克牌堆</p>
+     *
+     * @param nums
+     * @return
+     */
+    public int lengthOfLIS2(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] top = new int[nums.length];
+        top[0] = nums[0];
+        int p = 1;
+        for (int x : nums) {
+            int left = 0;
+            int right = p - 1;
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (top[mid] >= x) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            if (top[right] >= x) {
+                top[right] = x;
+            } else {
+                top[p] = x;
+                p++;
+            }
+        }
+        return p;
+    }
+
+
+    /**
+     * 俄罗斯套娃信封问题
+     * <p>先排序：先按a[0]递增排序，a[0]相同时再按a[1]递减排序</p>
+     * <P>后等同于最长递增子序列</P>
+     *
+     * @param envelopes
+     * @return
+     */
+    public int maxEnvelopes(int[][] envelopes) {
+        if (envelopes.length == 0) {
+            return 0;
+        }
+
+        Arrays.sort(envelopes, (a, b) -> (a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]));
+        int[] dp = new int[envelopes.length];
+        dp[0] = envelopes[0][1];
+        int res = 1;
+        for (int[] x : envelopes) {
+            int left = 0;
+            int right = res - 1;
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (dp[mid] >= x[1]) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            if (dp[right] >= x[1]) {
+                dp[right] = x[1];
+            } else {
+                dp[res] = x[1];
+                res++;
+            }
+        }
+        return res;
+    }
+
+
     public static void main(String[] args) {
         Solution s = new Solution();
         ListNode head = new ListNode(1);
