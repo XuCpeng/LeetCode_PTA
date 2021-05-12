e3916f7010d8b389e88175d9385f9cd6fd5b38d3
Stardust <saber3555@outlook.com>
2021-05-12 16:43:20 +0800

    update

--- JavaCodeSpace/src/cn/medemede/leecode/Solution.java
+++ JavaCodeSpace/src/cn/medemede/leecode/Solution.java
@@ -2901,6 +2901,53 @@ public class Solution {
         return false;
     }
 
+    /**
+     * 子集
+     * <p>旧子集添加新元素组成新的子集</p>
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new LinkedList<>());
+        for (int num : nums) {
+            int size = res.size();
+            for (int j = 0; j < size; j++) {
+                LinkedList<Integer> newSubSet = new LinkedList<>(res.get(j));
+                newSubSet.add(num);
+                res.add(newSubSet);
+            }
+        }
+        return res;
+    }
+
+    /**
+     * 子集
+     * <p>回溯，递归</p>
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> subsets2(int[] nums) {
+        res = new ArrayList<>();
+        getSubsets(0, nums, new LinkedList<>());
+        return res;
+    }
+
+    private void getSubsets(int i, int[] nums, LinkedList<Integer> track) {
+        if (i == nums.length) {
+            res.add(new LinkedList<>(track));
+            return;
+        }
+
+        getSubsets(i + 1, nums, track);
+        track.add(nums[i]);
+        getSubsets(i + 1, nums, track);
+        track.removeLast();
+    }
+
+
     public static void main(String[] args) {
         Solution s = new Solution();
         System.out.println(s.canPartitionKSubsets(new int[]{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3}, 8));
