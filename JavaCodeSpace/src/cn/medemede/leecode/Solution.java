23f6e2c8ef9450c18de663e33dd8842e3ab9673e
Stardust <saber3555@outlook.com>
2021-05-18 10:52:28 +0800

    位操作

--- JavaCodeSpace/src/cn/medemede/leecode/Solution.java
+++ JavaCodeSpace/src/cn/medemede/leecode/Solution.java
@@ -3369,13 +3369,68 @@ public class Solution {
         return res;
     }
 
+    /**
+     * 2的幂数
+     * <p>n&(n-1)可消除n的最后一位1
+     *
+     * @param n
+     * @return
+     */
     public boolean isPowerOfTwo(int n) {
         if (n <= 0) return false;
         return (n & (n - 1)) == 0;
+    }
+
+    /**
+     * 只出现一次的数字
+     * <p>a^a=0, a^0=a</p>
+     *
+     * @param nums
+     * @return
+     */
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int x : nums) {
+            res ^= x;
+        }
+        return res;
+    }
+
+    /**
+     * 两个只出现一次的数字
+     * <p>对整个数组求异或值count，利用count为1的位，对数组进行分组。</p>
+     * <p>分组结果可以满足：1. 相同数字在同一组；2.两个只出现一次的数字在不同组。</p>
+     *
+     * @param nums
+     * @return
+     */
+    public int[] singleNumber2(int[] nums) {
+        int count = 0;
+        for (int x : nums) {
+            count ^= x;
+        }
+        int i = 1;
+        for (int j = 0; j < 32; j++) {
+            if ((count & i) > 0) {
+                break;
+            }
+            i = i << 1;
+        }
+        int res1 = 0;
+        int res2 = 0;
+        for (int x : nums) {
+            if ((x & i) == 0) {
+                res1 ^= x;
+            } else {
+                res2 ^= x;
+            }
+        }
+        return new int[]{res1, res2};
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
         System.out.println(s.slidingPuzzle(new int[][]{{3, 2, 4}, {1, 5, 0}}));
     }
+
 }
