14f0cff6515aff90c2de4f26f436d6e7866228e5
Stardust <saber3555@outlook.com>
2021-06-17 11:58:04 +0800

    有效数字

---
+++ JavaCodeSpace/src/cn/medemede/leecode/IsNumber.java
@@ -0,0 +1,65 @@
+package cn.medemede.leecode;
+
+/**
+ * 有效数字
+ */
+public class IsNumber {
+    int i;
+    int n;
+    char[] cs;
+
+    public boolean isNumber(String s) {
+        cs = s.toCharArray();
+        i = 0;
+        n = cs.length;
+
+        if (!isDecimal()) {
+            i = 0;
+            if (isNotInteger()) {
+                return false;
+            }
+        }
+
+        if (i < n && (cs[i] == 'e' || cs[i] == 'E')) {
+            i++;
+            if (isNotInteger()) {
+                return false;
+            }
+        }
+
+        return i == n;
+    }
+
+    private boolean isDecimal() {
+        if (i < n && (cs[i] == '+' || cs[i] == '-')) {
+            i++;
+        }
+        int tmp = i;
+        while (i < n && ('0' <= cs[i] && cs[i] <= '9')) {
+            i++;
+        }
+        if (i < n && cs[i] == '.') {
+            i++;
+        } else {
+            return false;
+        }
+        while (i < n && ('0' <= cs[i] && cs[i] <= '9')) {
+            i++;
+        }
+        return i > tmp + 1;
+    }
+
+    private boolean isNotInteger() {
+        if (i < n && (cs[i] == '+' || cs[i] == '-')) {
+            i++;
+        }
+        if (i < n && ('0' <= cs[i] && cs[i] <= '9')) {
+            while (i < n && ('0' <= cs[i] && cs[i] <= '9')) {
+                i++;
+            }
+        } else {
+            return true;
+        }
+        return false;
+    }
+}
