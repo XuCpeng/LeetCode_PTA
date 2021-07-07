e1e3c82efdaf47a9640a0de7491f5616047e5fa0
Stardust <saber3555@outlook.com>
2021-07-07 11:43:52 +0800

    大餐计数

---
+++ JavaCodeSpace/src/cn/medemede/leecode/CountPairs.java
@@ -0,0 +1,27 @@
+package cn.medemede.leecode;
+
+import java.util.HashMap;
+
+/**
+ * 大餐计数
+ */
+public class CountPairs {
+    public int countPairs(int[] deliciousness) {
+        int res = 0;
+        HashMap<Integer, Integer> baseToNum = new HashMap<>();
+        int maxVal = 0;
+        for (int x : deliciousness) {
+            maxVal = Math.max(maxVal, x);
+        }
+        maxVal *= 2;
+        int base = 1000000007;
+        for (int j : deliciousness) {
+            for (int sum = 1; sum <= maxVal; sum <<= 1) {
+                int count = baseToNum.getOrDefault(sum - j, 0);
+                res = (res + count) % base;
+            }
+            baseToNum.put(j, baseToNum.getOrDefault(j, 0) + 1);
+        }
+        return res;
+    }
+}
