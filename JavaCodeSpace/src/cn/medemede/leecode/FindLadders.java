2521bf13974b8d9fee7fb542f8ababaecfa27968
Stardust <saber3555@outlook.com>
2021-08-30 16:25:28 +0800

    单词转换（受益匪浅）

--- JavaCodeSpace/src/cn/medemede/leecode/FindLadders.java
+++ JavaCodeSpace/src/cn/medemede/leecode/FindLadders.java
@@ -1,73 +1,58 @@
 package cn.medemede.leecode;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
+/**
+ * 面试题 17.22. 单词转换
+ * <p>
+ * 用HashMap记录前驱；用used数组标记已使用的元素，不可重复使用；数组中可能含有beginWord，必须判断，否则成环。
+ */
 public class FindLadders {
-    private boolean diff(String s1, String s2) {
-        if (s1.length() != s2.length()) {
-            return false;
-        }
-        int diffIndex = 0;
-        for (int i = 0; diffIndex < 2 && i < s1.length(); i++) {
-            if (s1.charAt(i) != s2.charAt(i)) {
-                diffIndex++;
-            }
-        }
-        return diffIndex ==1;
-    }
-
     public List<String> findLadders(String beginWord, String endWord, List<String> wordList) {
-        List<String> res = new ArrayList<>();
-        Collections.sort(wordList);
-        int i = 0;
-        while (i < wordList.size()) {
-            if (wordList.get(i).equals(endWord)) {
+        int n = wordList.size();
+        LinkedList<String> res = new LinkedList<>();
+        LinkedList<String> queue = new LinkedList<>();
+        boolean[] used = new boolean[n];
+        queue.addLast(beginWord);
+        HashMap<String, String> wordToPrev = new HashMap<>();
+
+        while (!queue.isEmpty()) {
+            String str = queue.pollFirst();
+            if (str.equals(endWord)) {
+                String resStr = endWord;
+                while (resStr != null) {
+                    res.addFirst(resStr);
+                    resStr = wordToPrev.get(resStr);
+                }
                 break;
             }
-            i++;
+            for (int i = 0; i < n; i++) {
+                if (!used[i]) {
+                    String word = wordList.get(i);
+                    if (diffOne(str, word) && !word.equals(beginWord)) {
+                        used[i] = true;
+                        queue.addLast(word);
+                        wordToPrev.put(word, str);
+                    }
+                }
+            }
         }
-        if (i==wordList.size()) {
-            return new ArrayList<>();
-        }
-        res.add(beginWord);
-        String[] targets = new String[]{beginWord, endWord};
-        Arrays.sort(targets);
-        int j = i;
-        if (beginWord.equals(targets[0])) {
-            for (; j >= 0; j--) {
-                String wordStr = wordList.get(j);
-                if (diff(beginWord, wordStr)) {
-                    break;
-                }
-            }
-
-
-            for (int k = j; k < i; k++) {
-                //May Error: need fix
-                if (diff(wordList.get(k),endWord)) {
-                    return new ArrayList<>();
-                }
-            }
-            res.addAll(wordList.subList(j, i + 1));
-
-
-        } else {
-            for (; j < wordList.size(); j++) {
-                String wordStr = wordList.get(j);
-                if (diff(beginWord, wordStr)) {
-                    break;
-                }
-            }
-            for (int k = i; k < j; k++) {
-                if (!diff(wordList.get(k), wordList.get(k + 1))) {
-                    return new ArrayList<>();
-                }
-            }
-            List<String> tmpList = wordList.subList(i, j + 1);
-            Collections.reverse(tmpList);
-            res.addAll(tmpList);
-        }
-
         return res;
+    }
+
+    private boolean diffOne(String a, String b) {
+        int n = a.length();
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                sum++;
+                if (sum > 1) {
+                    return false;
+                }
+            }
+        }
+        return sum == 1;
     }
 }
